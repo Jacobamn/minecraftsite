79d6e6add257254cf418d94f075e3452d9ae0f53
BuildTools <unconfigured@null.spigotmc.org>
2020-05-15 11:08:39 +0200

    added the files from earlier

---
+++ html/js/minecraft.js
@@ -0,0 +1,27 @@
+    func();
+    var seconds = 5 /*SECONDS, UPDATE INTERVAL*/;
+    setInterval(function(){
+        func();
+    }, seconds * 1000);
+    function func(){
+        var ip = "concordia.mcserver.us";
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://eu.mc-api.net/v3/server/info/" + ip, true);
+        xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                        data = JSON.parse(xhr.responseText);
+                        if (data.cache.status) {
+                                document.getElementById("bar").innerHTML=data.players.online + "/" + data.players.max;
+                                document.getElementById("bar").style.width = Math.round(100*(data.players.online/data.players.max)) + "%";
+                        } else {
+                                document.getElementById("bar").innerHTML="Offline";
+                        }
+                }
+        }
+        xhr.send();
+    }
+    
+
+
+
+
