994a90a91ecc0c0d1bdeae1a3864dacabec39a7d
jacobamn <jamn@cfait.dk>
2020-06-11 12:33:46 +0200

    added favicon and made adress button copy to clipboard

--- html/js/minecraft.js
+++ html/js/minecraft.js
@@ -23,3 +23,10 @@
     
 
 
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
